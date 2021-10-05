32bdea8150c8988b048bcf9e0c0a3b32eb32cb48
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2021-10-05 12:59:02 -0300

    programação orientada a objetos

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//testando 
+//testando o script
+console.log('testando');
+
+
+const circle = {
+radius: 1,
+location: {
+    x: 1,
+    y: 1
+},
+isVisible: true,
+draw: function () {
+   console.log('draw');
+}
+    
+};
+
+circle.draw();
