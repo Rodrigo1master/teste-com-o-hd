63c3534e50c094c90c1b34783f7655bfc908ed7c
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2021-10-06 18:38:50 -0300

    verificar se os elementos sao positivos

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
+//every elements
 
-const courses = [
-    {id: 1 , name: 'node.js'},
-    {id: 2 , name: 'javaScript'},
-];
+const numbers = [1 , 2 ,3 ,4];
 
-courses.sort(function(a,b) {
-    const nameA = a.name.toLocaleLowerCase();
-    const nameB = b.name.toLocaleLowerCase();
-
-    if (a.name < b.name) return-1;
-    if (a.name > b.name) return 1;
-    return 0;
+const allPositive = numbers.every(function(value){
+ return value >= 0;
 });
 
-console.log(courses)
+console.log(allPositive)
+//first element
+
+const atLeastOnePositive = numbers.some(function(value) {
+    return value >=0;
+});
+
+
+
