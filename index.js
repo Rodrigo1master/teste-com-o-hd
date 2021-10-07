0260a02503dc72728d54536498003244464b35de
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2021-10-07 16:12:20 -0300

    function declaration, function expression

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 
-//every elements
+//function declaration
 
-const numbers = [1 , 2 ,3 ,4];
-
-const output = move(numbers, 0, 3);
-
-console.log(output);
-
-function move(array, index, offset) {
-    const position = index + offset;
-    if (position >= array.length || position < 0){
-        console.error('invalid offset.');
-    return;
+function walk () {
+    console.log('walk');
 }
 
-const output = [...array];
-const element = output.splice(index, 1) [0]
-output.splice(position, 0, element);
-return output;
-}
+//function expression
+
+let run = function() {
+    console.log('run')
+};
+
+let move = run;
+
+move();
+run();
+walk()
