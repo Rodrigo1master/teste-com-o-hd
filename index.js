4cec5931c690feff216c4789f78b5b078d32bf6d
Rodrigo do Vale <rodrigodovale2002@gmail.com>
2021-10-07 14:14:58 -0300

    mover um elemento de posição

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 //every elements
 
-const numbers = [1 , -2 ,3 ,4];
+const numbers = [1 , 2 ,3 ,4];
 
-const itens = numbers
- .filter(n => n >= 0)
- .map (n => ({value:n}))
+const output = move(numbers, 0, 3);
 
- console.log(itens)
+console.log(output);
 
+function move(array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0){
+        console.error('invalid offset.');
+    return;
+}
+
+const output = [...array];
+const element = output.splice(index, 1) [0]
+output.splice(position, 0, element);
+return output;
+}
